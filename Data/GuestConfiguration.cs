adf24ed7f1e9b6a5413a66cbbb25e593019aabd3
Lucas Goularte <goulartelucasbeluco@gmail.com>
2024-08-28 00:12:58 -0300

    feat: Added new migrations
    was created as migrations AddingValueObjectToRoom, AddingValueObjectToGuest and added ObjectValues

---
+++ Data/GuestConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities = Domain.Entities;
+
+namespace Data
+{
+    public class GuestConfiguration : IEntityTypeConfiguration<Entities.Guest>
+    {
+      public void Configure(EntityTypeBuilder<Entities.Guest> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.OwnsOne(x => x.DocumentId)
+                .Property(x => x.IdNumber);
+
+            builder.OwnsOne(x => x.DocumentId)
+                .Property(x => x.DocumentType);
+        }
+    }
+}
